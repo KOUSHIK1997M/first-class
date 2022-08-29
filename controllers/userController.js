00fb33ec21c12eaa1f6fda5ea897b0112056cf75
KOUSHIK MANDAL <51336050+KOUSHIK1997M@users.noreply.github.com>
2022-08-29 16:26:14 +0530

    auth-1

---
+++ controllers/userController.js
@@ -0,0 +1,91 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../modles/userModel");
+
+
+///User create.............
+
+const createUser = async function (abcd, xyz) {
+  let data = abcd.body;
+  let savedData = await userModel.create(data);
+  console.log(abcd.newAtribute);
+  xyz.send({ msg: savedData });
+};
+
+
+/// User Login.............................
+
+const loginUser = async function (req, res) {
+  let userName = req.body.emailId;
+  let password = req.body.password;
+
+
+
+  let user = await userModel.findOne({ emailId: userName, password: password });
+  if (!user)
+    return res.send({
+      status: false,
+      msg: "username or the password is not corerct",
+    });
+
+
+
+
+  let token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "thorium",
+      organisation: "FunctionUp",
+    },
+    "functionup-plutonium-very-very-secret-key"
+  );
+  res.setHeader("x-auth-token", token);
+  res.send({ status: true, token: token });
+};
+
+
+
+////  Get user Data.....................
+
+const getUserData = async function (req, res) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+  //If no token is present in the request header return error. This means the user is not logged in.
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+  console.log(token);
+
+
+  let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+  if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+
+
+  let userId = req.params.userId;
+  let userDetails = await userModel.findById(userId);
+  if (!userDetails) return res.send({ status: false, msg: "No such user exists" });
+  res.send({ status: true, data: userDetails });
+  // Note: Try to see what happens if we change the secret while decoding the token
+};
+
+
+//// Update User data ..............................
+
+const updateUser = async function (req, res) {
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+
+  //Return an error if no user with the given id exists in the db
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  res.send({ status: updatedUser, data: updatedUser });
+};
+
+
+
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
