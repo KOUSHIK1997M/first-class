e344bb96c4fa9cbc5094c8a870a6ac3b4f29923a
KOUSHIK MANDAL <51336050+KOUSHIK1997M@users.noreply.github.com>
2022-09-16 23:51:06 +0530

    all ok project_2

---
+++ src/models/collegeModel.js
@@ -0,0 +1,32 @@
+//{ name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false} }
+const mongoose = require("mongoose")
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+        unique : true,  
+        trim : true 
+    },
+
+    fullName: {
+        type : String,
+        required : true,
+        trim: true
+    },
+
+    logoLink : {
+        type : String,
+        required : true,
+        trim: true
+    },
+
+    isDeleted : {
+        type : Boolean,
+        default : false
+    }
+
+
+})
+
+
+module.exports = mongoose.model("collegeModel", collegeSchema)
